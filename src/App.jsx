adb777b28498b1e9148bd8632f57decc741c4c71
rohitr06 <mailrrohit2004@gmail.com>
2025-02-15 17:40:16 +0530

    Applied Dark Mode opiton

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,14 @@ export default function App(){
     return savedTasks ? JSON.parse(savedTasks):[];
     }
   )
+  const [darkMode,setDarkMode]=useState(()=>{
+    return localStorage.getItem('darkMode')==='true'?true:false;
+  }
+);
+  useEffect(()=>{
+    localStorage.setItem('darkMode',darkMode);
+    document.documentElement.classList.toggle('dark',darkMode);
+  },[darkMode])
 
   useEffect(()=>{
     localStorage.setItem("tasks",JSON.stringify(tasks));
@@ -36,15 +44,20 @@ export default function App(){
   }
   
   return (
-    <div className="p-4 bg-red-400 rounded-xl min-h-screen  w-full flex flex-col items center sm:p-6 md:p-8 lg:p-12">
-      
-        <h1 className='text-2xl font-bold text-center text-gray-800 mb-4'>To-Do App</h1>
+<div className={`p-4 rounded-xl min-h-screen w-full flex flex-col items-center sm:p-6 md:p-8 lg:p-12 ${darkMode ? 'bg-gray-900 text-white' : 'bg-red-400 text-black'} transition-all`}>
+<div className='flex  gap-4 items-center'> 
+        <h1 className='text-2xl font-bold text-center text-gray-800 mb-4  ml-auto dark:text-white '>To-Do App</h1>
+        <button  onClick={()=> {setDarkMode(!darkMode)
+          console.log(darkMode);
+        }} className='bg-gray-600 text-white  rounded-full text-sm w-14 h-14 ml-auto'>{darkMode?'Light':'Dark'}</button>
+        </div>
+       
      <Task addTask={addTask}/>
-     <ul className='rounded-lg bg-gray-300 px-4 py-2 mt-4'>
+     <ul className='rounded-lg bg-gray-300 px-4 py-2 mt-4 dark:bg-gray-800'>
       {tasks.length>0?(
       tasks.map((task)=>(
         <li key={task.id} className='border-b py-2 flex flex-col gap-3 justify-between items-center sm:flex-row '>
-          <span className='ml-4 font-serif font-semibold text-violet-800 text-xl'>{task.text} - {task.time}</span>
+          <span className='ml-4 font-serif font-semibold text-violet-800 dark:text-violet-400 text-xl'>{task.text} - {task.time}</span>
           <div className='flex gap-2 '>
             <button onClick={()=>{
             const updatedtask=prompt("Edit the task",task.text);
@@ -60,7 +73,7 @@ export default function App(){
         </div></li>
       ))
 
-      ):<p className='text-gray-500 text-center py-4'> No tasks added yet</p>}
+      ):<p className='text-gray-500 text-center py-4 dark:text-gray-300'> No tasks added yet</p>}
       </ul> 
 
       
